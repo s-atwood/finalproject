e2dd5de1b0a8661889bc4127f3bf8bb6b79b8705
Shawn Atwood <sdatwood4@gmail.com>
2023-07-11 03:05:43 -0400

    hamburger menu

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
