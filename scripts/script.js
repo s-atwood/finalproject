2c287b0a695e585fa85af7602e1c4f1736db99cc
Shawn Atwood <sdatwood4@gmail.com>
2023-07-26 21:54:47 -0400

    new files

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+/*
+  Name: Shawn Atwood
+  Date: 7/10/2023
+  Filename: scripts.js
+*/
+
 // Hamburger menu function
 function hamburger() {
   var menu = document.getElementById("menu-links");
@@ -7,3 +13,19 @@ function hamburger() {
     menu.style.display = "block";
   }
 }
+
+// JavaScript to handle the custom play button
+var audio = document.getElementById('audio');
+var playButton = document.getElementById('play-button');
+
+playButton.addEventListener('click', function() {
+    if (audio.paused) {
+        // Start audio playback if it's paused
+        audio.play();
+        playButton.textContent = 'Pause Audio';
+    } else {
+        // Pause audio if it's playing
+        audio.pause();
+        playButton.textContent = 'For an enhanced experience, enjoy audio from the game!';
+    }
+});
